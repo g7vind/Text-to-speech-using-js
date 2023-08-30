d338cbd43d293b4f1f75782fa573c83b7e0bfa0e
g7vind <codingacc408@gmail.com>
2023-08-30 22:22:51 +0530

    initial

---
+++ script.js
@@ -0,0 +1,49 @@
+const textarea=document.querySelector("textarea");
+const button=document.querySelector("button");
+const animationHTML = `
+  <div class="bar"></div>
+  <div class="bar"></div>
+  <div class="bar"></div>
+  <div class="bar"></div>
+  <div class="bar"></div>
+  <div class="bar"></div>
+  <div class="bar"></div>
+  <div class="bar"></div>
+  <div class="bar"></div>
+  <div class="bar"></div>
+`;
+let isSpeaking=true;
+function text2Speech(){
+        const synth = window.speechSynthesis;
+        const text = textarea.value;
+        if (!synth.speaking && text) {
+          const utternace = new SpeechSynthesisUtterance(text);
+          synth.speak(utternace);
+        }
+        if (text.length > 50) {
+          if (synth.speaking && isSpeaking) {
+            button.innerText = "Pause";
+            synth.resume();
+            isSpeaking = false;
+          } else {
+            button.innerText = "Resume";
+            synth.pause();
+            isSpeaking = true;
+          }
+        } else {
+          isSpeaking = false;
+          button.innerHTML=animationHTML;
+        }
+        setInterval(() => {
+          if (!synth.speaking && !isSpeaking) {
+            isSpeaking = true;
+            button.innerText = "Convert to Speech";
+          }
+        });
+}
+window.addEventListener('beforeunload', () => {
+    if (synth.speaking) {
+        synth.cancel();
+    }
+});
+
