989dd2bca479d315f15dad6c2ce16350c264a54b
g7vind <codingacc408@gmail.com>
2023-08-30 22:50:04 +0530

    update

--- script.js
+++ script.js
@@ -16,29 +16,34 @@ let isSpeaking=true;
 function text2Speech(){
         const synth = window.speechSynthesis;
         const text = textarea.value;
-        if (!synth.speaking && text) {
-          const utternace = new SpeechSynthesisUtterance(text);
-          synth.speak(utternace);
-        }
-        if (text.length > 50) {
-          if (synth.speaking && isSpeaking) {
-            button.innerText = "Pause";
-            synth.resume();
+        if(text.length>0){
+            if (!synth.speaking && text) {
+            const utternace = new SpeechSynthesisUtterance(text);
+            synth.speak(utternace);
+            }
+            if (text.length > 50) {
+            if (synth.speaking && isSpeaking) {
+                button.innerText = "Pause";
+                synth.resume();
+                isSpeaking = false;
+            } else {
+                button.innerText = "Resume";
+                synth.pause();
+                isSpeaking = true;
+            }
+            } else {
             isSpeaking = false;
-          } else {
-            button.innerText = "Resume";
-            synth.pause();
-            isSpeaking = true;
-          }
-        } else {
-          isSpeaking = false;
-          button.innerHTML=animationHTML;
+            button.innerHTML=animationHTML;
+            }
+            setInterval(() => {
+            if (!synth.speaking && !isSpeaking) {
+                isSpeaking = true;
+                button.innerText = "Convert to Speech";
+            }
+            });
         }
-        setInterval(() => {
-          if (!synth.speaking && !isSpeaking) {
-            isSpeaking = true;
-            button.innerText = "Convert to Speech";
-          }
-        });
+        else    
+            alert("Please enter some text");
+        
 }
 
